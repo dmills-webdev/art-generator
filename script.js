fcae10372ab46cbe6ca1c84ea5b0992cdf89833a
crongle <gasmazk73@gmail.com>
2020-04-26 15:29:25 +0100

    Optimise JS to remove unneeded re-draws

--- script.js
+++ script.js
@@ -1,67 +1,96 @@
 ////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////// Canvas
+////////////////////////////////////////////////////////////////////////////////
 const canvas = (() => {
-//Local Variables
+// Local Variables
   const canvas = {};
   const webCanvas = document.getElementById("canvas");
   canvas.cell = [0,1,2,3,4,5,6,7,8,9];
+  canvas.previousx = null;
+  canvas.previousy= null;
 
 
-
-//Takes inputs on the design of the next canvas
+// Takes inputs on the design of the next canvas
   canvas.getDesign = () => {
     let x = document.getElementById("grid-width").value;
     let y = document.getElementById("grid-height").value;
 
     if (x && y == 0) {  //Default values incase inputs are left empty
-      canvas.x = 20;
-      canvas.y = 20;
+      canvas.x = 10;
+      canvas.y = 10;
     }
     else {              //Otherwise take user input
       canvas.x = x;
       canvas.y = y;
     }
+
+    if (canvas.checkForChange()) {
+      webCanvas.style.gridTemplateColumns=('repeat(' + canvas.x + ', ' + (800 / canvas.x) + 'px)');
+      webCanvas.style.gridTemplateRows=('repeat(' + canvas.y + ', ' + (800 / canvas.y) + 'px)');
+    }
   }
 
+// Checks if a canvas re-draw is neeed or not
+  canvas.checkForChange = () => {
+    if (canvas.previousx==canvas.x && canvas.previousy==canvas.y) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
 
-//Picks a random cell pattern from the options
-  canvas.randomCell = () => {
-    let i = Math.floor(Math.random()*10);
+// Picks a random cell pattern from the options
+  canvas.randomCellFill = () => {
+    let i = Math.floor(Math.random()*6); //CHANGE based on number of cell fill options
     return i;
   }
 
+////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////// Canvas creation
+////////////////////////////////////////////////////////////////////////////////
+// Controller for single canvas creation
 
-//Draws the canvas to the display
-  canvas.draw = () => {
-    canvas.clear();
+  canvas.new = () => {
     canvas.getDesign();
+    if (canvas.checkForChange()) {
+      canvas.previousx = canvas.x;
+      canvas.previousy = canvas.y;
+      canvas.drawGrid();
+    }
+    canvas.fillCells();
+  }
+
+// Create unstyled grid
+  canvas.drawGrid = () => {
     for (i = 0; i < (canvas.x*canvas.y); ++i) {
       const cell = document.createElement("div");
-
-      cell.classList.add("canvas-cell");
-      cell.classList.add("cell-fill-" + canvas.randomCell());
-      webCanvas.style.gridTemplateColumns=('repeat(' + canvas.x + ', ' + (800 / canvas.x) + 'px)');
-      webCanvas.style.gridTemplateRows=('repeat(' + canvas.y + ', ' + (800 / canvas.y) + 'px)');
-      webCanvas.appendChild(cell);
+      cell.classList.add("cell-fill-0");
+      webCanvas.appendChild(cell); // Adds grid of cells to DOM
     }
   }
 
+// Style the unstyled grid on display
+  canvas.fillCells = () => {
+    const regexp =  new RegExp(/\d\b/);
+    let cells = webCanvas.children;
 
-//Clears the current canvas on display
-  canvas.clear = () => {
-    let child = document.getElementsByClassName("canvas-cell");
-    while(child[0]) { // Loop removes the entirity of the previous display output
-      child[0].parentNode.removeChild(child[0]);
+    for (i = 0; i < (canvas.x*canvas.y); ++i) {
+      cells[i].className = cells[i].className.replace(regexp, canvas.randomCellFill());
     }
   }
+
   return canvas;
 })();
 
+////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////// Auto Generation Controller
 ////////////////////////////////////////////////////////////////////////////////
 const controller = (() => {
   const controller = {};
   controller.activeToggle = false;
 
-
+// Starts auto generation if not currently active
   controller.startAuto = () => {
     if (controller.activeToggle == false) {
       controller.activeToggle = !controller.activeToggle;
@@ -72,21 +101,25 @@ const controller = (() => {
     }
   }
 
-
+// Stops auto generation
   controller.stopAuto = () => {
-    controller.activeToggle = !controller.activeToggle;
-    clearInterval(controller.autoGenerate);
+    if (controller.activeToggle == true) {
+      controller.activeToggle = !controller.activeToggle;
+      clearInterval(controller.autoGenerate);
+    }
+    else {
+      return;
+    }
+
   }
-
-
+// Starts auto generation
   controller.auto = () => {
     let autoInterval = (document.getElementById("auto-interval").value * 1000);
-    canvas.draw()
-    controller.autoGenerate = setInterval(canvas.draw,autoInterval);
+    canvas.new();
+    controller.autoGenerate = setInterval(canvas.new, autoInterval);
   }
 
-
-  return controller
+  return controller;
 })();
 
-canvas.draw();
+canvas.new();
